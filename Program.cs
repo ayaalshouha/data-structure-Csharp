13ac82bdf64eb50ecfc8e9461852d8f590e457d2
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-03-22 16:27:36 +0300

    Lists<datatype>

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace Datastructure_C_
     {
         static void Main(string[] args)
         {
+            //Lists using C# 
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+                
+            list.Add(3);
+            list.Add(4);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
         }
     }
 }
