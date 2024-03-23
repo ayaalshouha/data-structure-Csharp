63ff4b692fdf67b0b2dfa4b1b4e00574f83b40b5
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-03-23 16:37:46 +0300

    looping through lists

--- Program.cs
+++ Program.cs
@@ -11,21 +11,31 @@ namespace Datastructure_C_
     {
         static void Main(string[] args)
         {
+
             //Lists using C# 
             List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Console.WriteLine("list : " + string.Join(" ", list1));
+            
+            //looping through lists 
+            foreach (int i in list1)
+                Console.Write(i + " ");
 
-            list1.RemoveAll(n => n % 2 == 0);
-             Console.WriteLine();
-             Console.WriteLine("new list (Odd Numbers): " + string.Join(" ", list1));
-
-            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            list.RemoveAll(x => x % 2 != 0);
             Console.WriteLine();
-            Console.WriteLine("new list (Even Numbers): " + string.Join(" ", list));
 
+            //using labda expression
+            list1.ForEach(number=>  Console.Write(number + " "));
+
+            /*Console.WriteLine("list : " + string.Join(" ", list1));
+
+              list1.RemoveAll(n => n % 2 == 0);
+               Console.WriteLine();
+               Console.WriteLine("new list (Odd Numbers): " + string.Join(" ", list1));
+
+              List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+              list.RemoveAll(x => x % 2 != 0);
+              Console.WriteLine();
+              Console.WriteLine("new list (Even Numbers): " + string.Join(" ", list));*/
 
             Console.ReadLine();
         }
