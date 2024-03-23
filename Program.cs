90b80a8886c77ea72b589cc2943fe6693e311dd3
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-03-23 16:19:03 +0300

    remove from lists

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,20 @@ namespace Datastructure_C_
         static void Main(string[] args)
         {
             //Lists using C# 
-            List<int> list = new List<int>();
-            list.Add(1);
-            list.Add(2);
-                
-            list.Add(3);
-            list.Add(4);
-            foreach (int i in list)
-            {
-                Console.WriteLine(i);
-            }
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("list : " + string.Join(" ", list1));
+
+            list1.RemoveAll(n => n % 2 == 0);
+             Console.WriteLine();
+             Console.WriteLine("new list (Odd Numbers): " + string.Join(" ", list1));
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            list.RemoveAll(x => x % 2 != 0);
+            Console.WriteLine();
+            Console.WriteLine("new list (Even Numbers): " + string.Join(" ", list));
+
 
             Console.ReadLine();
         }
