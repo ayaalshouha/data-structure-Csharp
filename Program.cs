6a83d627d5fc4646eb3634d89d5de6fe7e01ec74
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-03-24 15:19:35 +0300

    sorting a list

--- Program.cs
+++ Program.cs
@@ -13,17 +13,24 @@ namespace Datastructure_C_
         {
 
             //Lists using C# 
-            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> list1 = new List<int>() { 11, -20, 300, -450, 55, 610, -7, 358, 90, 3 };
 
             //using lambda expression
             list1.ForEach(number => Console.Write(number + " "));
+            Console.WriteLine();
+
+            //sorting a list
+            list1.Sort();
+            Console.WriteLine(string.Join(", ", list1));
+            list1.Reverse();
+            Console.WriteLine(string.Join(", ", list1)); 
 
 
-            Console.WriteLine("Even Numbers : " + string.Join(", ", list1.Where(n => n % 2 == 0)));
+           /* Console.WriteLine("Even Numbers : " + string.Join(", ", list1.Where(n => n % 2 == 0)));
             Console.WriteLine("Odd Numbers : " + string.Join(", ", list1.Where(n => n % 2 != 0)));
             Console.WriteLine("Numbers Greater Than 5 : " + string.Join(", ", list1.Where(n => n > 5)));
             Console.WriteLine("Numbers Between 3 and 8 : " + string.Join(", ", list1.Where(n => (n > 3 && n < 8))));
-            Console.WriteLine("Every Second Number : " + string.Join(", ", list1.Where((n,index) => index % 2 == 1)));     
+            Console.WriteLine("Every Second Number : " + string.Join(", ", list1.Where((n,index) => index % 2 == 1)));     */
 
 
             //looping through lists 
