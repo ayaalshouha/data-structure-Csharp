2b44a18d9d4aac55ba64a45e7fc5fb723c59f2a8
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-03-24 15:25:32 +0300

    sorting a list using LINQ

--- Program.cs
+++ Program.cs
@@ -25,12 +25,23 @@ namespace Datastructure_C_
             list1.Reverse();
             Console.WriteLine(string.Join(", ", list1)); 
 
+            //sorting a list using LINQ 
+            Console.WriteLine(string.Join(", ", list1.OrderBy(n=>n)));
+            Console.WriteLine(string.Join(", ", list1.OrderByDescending(n => n)));
 
-           /* Console.WriteLine("Even Numbers : " + string.Join(", ", list1.Where(n => n % 2 == 0)));
-            Console.WriteLine("Odd Numbers : " + string.Join(", ", list1.Where(n => n % 2 != 0)));
-            Console.WriteLine("Numbers Greater Than 5 : " + string.Join(", ", list1.Where(n => n > 5)));
-            Console.WriteLine("Numbers Between 3 and 8 : " + string.Join(", ", list1.Where(n => (n > 3 && n < 8))));
-            Console.WriteLine("Every Second Number : " + string.Join(", ", list1.Where((n,index) => index % 2 == 1)));     */
+
+
+
+
+
+
+
+
+            /* Console.WriteLine("Even Numbers : " + string.Join(", ", list1.Where(n => n % 2 == 0)));
+             Console.WriteLine("Odd Numbers : " + string.Join(", ", list1.Where(n => n % 2 != 0)));
+             Console.WriteLine("Numbers Greater Than 5 : " + string.Join(", ", list1.Where(n => n > 5)));
+             Console.WriteLine("Numbers Between 3 and 8 : " + string.Join(", ", list1.Where(n => (n > 3 && n < 8))));
+             Console.WriteLine("Every Second Number : " + string.Join(", ", list1.Where((n,index) => index % 2 == 1)));     */
 
 
             //looping through lists 
